ad4306044fcadc33c84546d0847bdc5eca62a3a2
chiawei913 <jeffrey0504tw@gmail.com>
2021-07-13 10:35:17 +0800

    購物網_front

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+b-container#home
+  b-row
+    b-col(cols="12" md="6" lg="4" v-for="product in products" :key="product._id")
+      router-link(:to="'/product/'+product._id")
+        ProductCard(:product="product")
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ProductCard from '@/components/ProductCard.vue'
 
 export default {
   name: 'Home',
+  data () {
+    return {
+      products: []
+    }
+  },
   components: {
-    HelloWorld
+    ProductCard
+  },
+  async mounted () {
+    try {
+      const { data } = await this.axios.get('/products')
+      this.products = data.result.map(product => {
+        if (product.image) {
+          product.image = `${process.env.VUE_APP_API}/files/${product.image}`
+        }
+        return product
+      })
+    } catch (error) {
+      this.$swal({
+        icon: 'error',
+        title: '錯誤',
+        text: '取得商品失敗'
+      })
+    }
   }
 }
 </script>
